8ced19f2acfd6fc1094b2eef1f382402a9318a91
Timo Wilken <timo.21.wilken@gmail.com>
2019-11-09 00:03:57 +0000

    Try drawing road tiles with different edges connected.

--- main.rkt
+++ main.rkt
@@ -35,6 +35,39 @@
       ['south (tile-pos x (+ y distance))]
       ['west (tile-pos (- x distance) y)])))
 
+(define (draw-real-point dc center color radius)
+  (send dc set-pen "black" 0 'transparent)
+  (send dc set-brush color 'solid)
+  (let ([top-left-x (- (tile-pos-x center) radius)]
+        [top-left-y (- (tile-pos-y center) radius)]
+        [diameter (* 2 radius)])
+    (send dc draw-ellipse top-left-x top-left-y diameter diameter)))
+
+(define/match (draw-road-tile dc tile connect-directions)
+  [(dc (tile-pos x y) connect-directions)
+   (define (draw-center-point)
+     (let ([center-pos (tile-anchor-coords (subtile-pos x y 'center))])
+       (draw-real-point dc center-pos "black" 1/4)
+       (draw-real-point dc center-pos "white" 1/32)))
+
+   (match connect-directions
+     [(list) (void)]
+     [(list-no-order 'center other-dir)
+      (begin (draw-center-point)
+             (send dc set-pen (send the-pen-list find-or-create-pen "black" 1/2 'solid 'butt))
+             (match-let ([(tile-pos x1 y1) (tile-anchor-coords (subtile-pos x y 'center))]
+                         [(tile-pos x2 y2) (tile-anchor-coords (subtile-pos x y other-dir))])
+               (send dc draw-line x1 y1 x2 y2)
+               (send dc set-pen (send the-pen-list find-or-create-pen "white" 1/16 'solid 'butt))
+               (send dc draw-line x1 y1 x2 y2)))]
+     [(list-no-order 'center other-dirs ...) (draw-road-tile dc tile other-dirs)]
+     [(list-no-order 'north 'south 'east 'west)
+      (begin empty)]
+     [(list-no-order 'east 'west)
+      (begin empty)]
+     [(list-no-order 'north 'south)
+      (begin empty)])])
+
 (define (draw-road dc direction from-tile-x from-tile-y for-tiles)
   (match-let* ([(tile-pos from-point-x from-point-y) (tile-anchor-coords (subtile-pos from-tile-x from-tile-y direction))]
                [(tile-pos to-point-x to-point-y) (move-coords (tile-pos from-point-x from-point-y) direction for-tiles)])
@@ -53,8 +86,16 @@
   (for*/list ([x (range 8)] [y (range 6 10)])
     (draw-base-tile dc 'water (tile-pos x y)))
   (draw-road dc 'south 1 1 3)
-  (draw-road dc 'east 2 1 5))
+  (draw-road dc 'east 2 1 5)
+  (draw-road-tile dc (tile-pos 3 3) '(center east))
+  (draw-road-tile dc (tile-pos 4 3) '(west east))
+  (draw-road-tile dc (tile-pos 4 4) '(north south west east)))
 
-(let ([frame (new frame% [label "TinyCity"] [width 640] [height 480])])
-  (new canvas% [parent frame] [paint-callback main-canvas-paint])
+(let* ([frame (new frame% [label "TinyCity"] [width 640] [height 480])]
+       [canvas (new canvas% [parent frame]
+                    ;; This uses a lambda so that main-canvas-paint can be
+                    ;; updated while the program is running!
+                    [paint-callback (λ args (apply main-canvas-paint args))]
+                    [style '(hscroll vscroll)])])
+  (send canvas init-auto-scrollbars (* 25 *tile-size*) (* 25 *tile-size*) 0 0)
   (send frame show #t))
