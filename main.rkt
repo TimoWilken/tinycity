6f914efa965828b09b54aec8b7b2fb3bab2989d6
Timo Wilken <timo.21.wilken@gmail.com>
2019-11-10 15:14:14 +0000

    Simple but working road drawing; first attempt at storing the world.

--- main.rkt
+++ main.rkt
@@ -7,8 +7,12 @@
 (define *show-tile-grid* #t)
 (define *terrain-colors*
   #hash((grass . "green") (concrete . "gray") (water . "darkblue")))
+(define *asphalt-pen*
+  (send the-pen-list find-or-create-pen "black" 1/2 'solid 'butt))
+(define *road-markings-pen*
+  (send the-pen-list find-or-create-pen "white" 1/16 'solid 'butt))
 
-(define (draw-base-tile dc type x y)
+(define (draw-base-tile dc x y type)
   (send dc set-pen "brown" 1/64 (if *show-tile-grid* 'solid 'transparent))
   (send dc set-brush (hash-ref *terrain-colors* type) 'solid)
   (send dc draw-rectangle x y 1 1))
@@ -34,69 +38,107 @@
         [diameter (* 2 radius)])
     (send dc draw-ellipse top-left-x top-left-y diameter diameter)))
 
-(define (make-road-tile-bitmap connect-directions)
-  (define/match (angle-from-north direction)
-    [('north) 0]
-    [('east ) (* 1/2 pi)]
-    [('south) pi]
-    [('west ) (* 3/2 pi)])
-
-  (let* ([bitmap (make-bitmap *tile-size* *tile-size*)]
-         [dc (send bitmap make-dc)])
-    (send dc set-scale *tile-size* *tile-size*)
-    (send dc set-smoothing 'aligned)
-    (match connect-directions
-      ['() bitmap]
-
-      [(list-no-order 'center other-dir)
-       (draw-real-point dc 1/2 1/2 "black" 1/4)
-       (draw-real-point dc 1/2 1/2 "white" 1/32)
-       (send dc set-rotation (angle-from-north other-dir))
-       (send dc set-pen (send the-pen-list find-or-create-pen "black" 1/2 'solid 'butt))
-       (send dc draw-line 1/2 1/2 1/2 0)
-       (send dc set-pen (send the-pen-list find-or-create-pen "white" 1/16 'solid 'butt))
-       (send dc draw-line 1/2 1/2 1/2 0)
-       bitmap]
-
-      [(list-no-order 'center other-dirs ...)
-       (make-road-tile-bitmap other-dirs)]
-
-      [(list-no-order 'north 'south 'east 'west)
-       bitmap])))
-
 (define (draw-road-tile dc x y connect-directions)
-  (send dc set-scale 1 1)
-  (send dc draw-bitmap (make-road-tile-bitmap connect-directions) (* x *tile-size*) (* y *tile-size*))
-  (send dc set-scale *tile-size* *tile-size*))
+  (match connect-directions
+    ['() (void)]
+    [(list-no-order 'center other-dirs ...)
+     (draw-road-tile dc x y other-dirs)]
+    [(list-no-order dir other-dirs ...)
+     (let-values ([(ctr-x ctr-y) (tile-anchor-coords x y 'center)]
+                  [(edge-x edge-y) (tile-anchor-coords x y dir)])
+       (draw-real-point dc ctr-x ctr-y "black" 1/4)
+       (send dc set-pen *asphalt-pen*)
+       (send dc draw-line ctr-x ctr-y edge-x edge-y)
+       (draw-road-tile dc x y other-dirs)
+       (draw-real-point dc ctr-x ctr-y "white" 1/32)
+       (send dc set-pen *road-markings-pen*)
+       (send dc draw-line ctr-x ctr-y edge-x edge-y))]))
 
-(define (draw-road dc direction from-tile-x from-tile-y for-tiles)
-  (let*-values ([(from-point-x from-point-y) (tile-anchor-coords from-tile-x from-tile-y direction)]
-                [(to-point-x to-point-y) (move-coords from-point-x from-point-y direction for-tiles)])
-    (send dc set-pen "black" 1/2 'solid)
-    (send dc draw-line from-point-x from-point-y to-point-x to-point-y)
-    (send dc set-pen "white" 1/16 'solid)
-    (send dc draw-line from-point-x from-point-y to-point-x to-point-y)))
+(define (draw-world dc world)
+  (for ([(pos tile) (in-hash world)])
+    (match-let ([(list x y) pos]
+                [(list base cover-info ...) tile])
+      (draw-base-tile dc x y base)
+      (draw-tile-cover dc x y cover-info))))
+
+(define (draw-tile-cover dc x y cover-info)
+  (match cover-info
+    ['() (void)]
+    [(list 'power other ...)
+     (draw-tile-cover dc x y other)
+     (void)] ;; TODO draw power lines
+    [(list 'zone zone-type)
+     (void)] ;; TODO draw zone
+    [(list 'road connected-dirs ...)
+     (draw-road-tile dc x y connected-dirs)]
+    [(list 'building building-type)
+     (void)])) ;; TODO draw building
+
+(define (create-road! world x y connect-dirs)
+  (unless (can-build-at? world x y 'road)
+    (error 'create-road! "can't build road here"))
+  (hash-update!
+   world (list x y)
+   (match-lambda
+     [(list base 'road connected-dirs ...)
+      `(,base road . ,(set-union connected-dirs connect-dirs))]
+     [(list base _ ...)
+      ;; overwrite with or add road -- TODO is this the right thing to do?
+      `(,base road . ,connect-dirs)])))
+
+(define (can-build-on-tile? tile new-type)
+  (match tile
+    ['() #f]
+    [(list base) (not (eq? base 'water))]
+    [(list _ 'road _ ...)
+     (match new-type [(or 'road 'power) #t] [_ #f])]
+    [(list _ 'building _ ...) #f]
+    [(list _ 'zone _) #f]
+    [(list base 'power other ...)
+     (can-build-on-tile? (cons base other) new-type)]))
+
+(define (can-build-at? world x y type)
+  (can-build-on-tile? (hash-ref world (list x y) '()) type))
+
+(define *world*
+  (let ([world (make-hash)])
+    (for* ([x (range 10)] [y (range 6)])
+      (hash-set! world (list x y)
+                 (list (if (and (<= 3 x 6) (<= 3 y 5)) 'concrete 'grass))))
+    (for* ([x (range 10)] [y (range 6 10)])
+      (hash-set! world (list x y) (list 'water)))
+    (for ([x (range 5 9)])
+      (create-road! world x 2 '(east west)))
+    (create-road! world 3 3 '(center east))
+    (create-road! world 4 3 '(west south))
+    (create-road! world 4 4 '(north east south))
+    world))
 
 (define (main-canvas-paint canvas dc)
   (send dc set-scale *tile-size* *tile-size*)
   (send dc set-smoothing 'aligned)
-  (for*/list ([x (range 8)] [y (range 6)])
-    (draw-base-tile dc 'grass x y))
-  (for*/list ([x (range 3 6)] [y (range 3 5)])
-    (draw-base-tile dc 'concrete x y))
-  (for*/list ([x (range 8)] [y (range 6 10)])
-    (draw-base-tile dc 'water x y))
-  (draw-road dc 'south 1 1 3)
-  (draw-road dc 'east 2 1 5)
-  (draw-road-tile dc 3 3 '(center east))
-  ;(draw-road-tile dc 4 3 '(west east))
-  (draw-road-tile dc 4 4 '(north south west east)))
+  (draw-world dc *world*))
+
+(define main-canvas%
+  (class canvas%
+    (init-field toplevel)
+    (when (not (send toplevel has-status-line?))
+      (send toplevel create-status-line))
+
+    (define/override (on-event event)
+      (send toplevel set-status-text
+            (format "(~a, ~a) px; tile (~a, ~a) ~a"
+                    (send event get-x) (send event get-y)
+                    (quotient (send event get-x) *tile-size*)
+                    (quotient (send event get-y) *tile-size*)
+                    (send event get-event-type))))
+
+    (super-new [style '(hscroll vscroll)]
+               ;; This uses a lambda so that main-canvas-paint can be
+               ;; updated while the program is running!
+               [paint-callback (λ args (apply main-canvas-paint args))])))
 
 (let* ([frame (new frame% [label "TinyCity"] [width 640] [height 480])]
-       [canvas (new canvas% [parent frame]
-                    ;; This uses a lambda so that main-canvas-paint can be
-                    ;; updated while the program is running!
-                    [paint-callback (λ args (apply main-canvas-paint args))]
-                    [style '(hscroll vscroll)])])
+       [canvas (new main-canvas% [parent frame] [toplevel frame])])
   (send canvas init-auto-scrollbars (* 25 *tile-size*) (* 25 *tile-size*) 0 0)
   (send frame show #t))
