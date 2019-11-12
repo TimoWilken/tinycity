13508292809af1326e920f82f9530bc8b93e38b8
Timo Wilken <timo.21.wilken@gmail.com>
2019-11-12 23:36:39 +0000

    Try profiling slow redraws-on-scroll.
    It seems like dynamically creating pens and brushes every time they're used
    takes up ~20% of processing time, so extract them to top-level definitions.

--- main.rkt
+++ main.rkt
@@ -1,14 +1,28 @@
 #!/usr/bin/racket
 #lang racket
+(require profile)
 (require racket/gui)
 (require racket/draw)
 
 (define *tile-size* 32)
 (define *world-size* 100)
-(define *show-tile-grid* #f)
 (define *scroll-step* 1/32)
-(define *terrain-colors*
-  #hash((grass . "green") (concrete . "gray") (water . "darkblue")))
+(define *terrain-brushes*
+  (make-immutable-hash
+   (hash-map #hash((grass . "green")
+                   (concrete . "gray")
+                   (water . "darkblue"))
+             (λ (terrain color)
+               (cons terrain (send the-brush-list
+                                   find-or-create-brush color 'solid))))))
+(define *transparent-pen*
+  (send the-pen-list find-or-create-pen "black" 0 'transparent))
+(define *tile-grid-pen*
+  (send the-pen-list find-or-create-pen "brown" 1/64 'transparent))
+(define *cursor-tile-brush*
+  (send the-brush-list find-or-create-brush "black" 'transparent))
+(define *cursor-tile-pen*
+  (send the-pen-list find-or-create-pen "red" 1/32 'solid))
 (define *asphalt-pen*
   (send the-pen-list find-or-create-pen "black" 1/2 'solid 'butt))
 (define *road-markings-pen*
@@ -45,17 +59,12 @@
     [(list _ 'road _ ...) #t]
     [_ #f]))
 
-(define (draw-base-tile dc x y type)
-  (send dc set-pen "brown" 1/64 (if *show-tile-grid* 'solid 'transparent))
-  (send dc set-brush (hash-ref *terrain-colors* type) 'solid)
-  (send dc draw-rectangle x y 1 1))
-
 (define (draw-real-point dc x y color radius)
   ;; (send dc draw-point ...) doesn't draw a point/circle, but a short line.
   ;; Because we've scaled the DC by a large number, this line is very visible.
   ;; Hence, we need to draw the "point" using an ellipse. The disadvantage is
   ;; that we can't use the current pen!
-  (send dc set-pen "black" 0 'transparent)
+  (send dc set-pen *transparent-pen*)
   (send dc set-brush color 'solid)
   (let ([top-left-x (- x radius)]
         [top-left-y (- y radius)]
@@ -84,7 +93,9 @@
 
 (define (draw-tile dc x y tile-info)
   (match-let ([(list base cover-info ...) tile-info])
-    (draw-base-tile dc x y base)
+    (send dc set-pen *tile-grid-pen*)
+    (send dc set-brush (hash-ref *terrain-brushes* base))
+    (send dc draw-rectangle x y 1 1)
     (let draw-tile-cover ([cover-info cover-info])
       (match cover-info
         ['() (void)]
@@ -158,24 +169,26 @@
     world))
 
 (define (draw-cursor-tile dc x y)
-  (define pen-width 1/32)
-  (send dc set-brush "black" 'transparent)
-  (send dc set-pen "red" pen-width 'solid)
-  (send dc draw-rectangle
-        (+ x (/ pen-width 2)) (+ y (/ pen-width 2))
-        (- 1 pen-width) (- 1 pen-width)))
+  (send dc set-brush *cursor-tile-brush*)
+  (send dc set-pen *cursor-tile-pen*)
+  (let ([pen-width (send *cursor-tile-pen* get-width)])
+    (send dc draw-rectangle
+          (+ x (/ pen-width 2)) (+ y (/ pen-width 2))
+          (- 1 pen-width) (- 1 pen-width))))
 
 (define (main-canvas-paint canvas dc)
-  (send dc set-scale *tile-size* *tile-size*)
-  (send dc set-smoothing 'smoothed)
-  (let*-values ([(left-px top-px) (send canvas get-view-start)]
-                [(width-px height-px) (send canvas get-client-size)]
-                [(left top) (pixels->tiles left-px top-px)]
-                [(right bottom) (pixels->tiles (+ left-px width-px) (+ top-px height-px))])
-    (for ([(pos tile) *world*])
-      (match-let ([(list x y) pos])
-        (when (and (<= left x right) (<= top y bottom))
-          (draw-tile dc x y tile))))))
+  (profile #:delay 0 #:repeat 500 #:order 'self
+   (begin
+     (send dc set-scale *tile-size* *tile-size*)
+     (send dc set-smoothing 'smoothed)
+     (let*-values ([(left-px top-px) (send canvas get-view-start)]
+                   [(width-px height-px) (send canvas get-client-size)]
+                   [(left top) (pixels->tiles left-px top-px)]
+                   [(right bottom) (pixels->tiles (+ left-px width-px) (+ top-px height-px))])
+       (for ([(pos tile) *world*])
+         (match-let ([(list x y) pos])
+           (when (and (<= left x right) (<= top y bottom))
+             (draw-tile dc x y tile))))))))
 
 (define (draw-world-tile dc world x y)
   (let ([tile (hash-ref *world* (list x y) #f)])
@@ -186,6 +199,22 @@
   (for ([tile (tile-neighbours x y)])
     (match-let ([(list _ nx ny) tile])
       (draw-world-tile dc world nx ny))))
+
+(define (tool-handle-event tool event dc)
+  (define-values (tx ty)
+    (pixels->tiles (send event get-x) (send event get-y)))
+  (when (send event button-up? 'left)
+    (match tool
+      ['(build road)
+       (when (with-handlers ([exn:fail:contract? (λ (e) #f)])
+               (place-road! *world* tx ty) #t)
+         (draw-tile-with-neighbours dc *world* tx ty))]
+      ['(bulldoze)
+       (remove-road! *world* tx ty)
+       (draw-tile-with-neighbours dc *world* tx ty)]
+      [(list 'build 'zone zone-type)
+       (void)]
+      [_ (void)])))
 
 (define main-canvas%
   (class canvas%
@@ -207,18 +236,7 @@
       (match previous-cursor-tile
         [#f (void)]
         [(list x y) (draw-world-tile (get-dc) *world* x y)])
-
-      (when (send event button-up? 'left)
-        (match (get-selected-tool)
-          ['(build road)
-           (when (with-handlers ([exn:fail:contract? (λ (e) #f)])
-                   (place-road! *world* tx ty) #t)
-             (draw-tile-with-neighbours (get-dc) *world* tx ty))]
-          ['(bulldoze)
-           (remove-road! *world* tx ty)
-           (draw-tile-with-neighbours (get-dc) *world* tx ty)]
-          [_ (void)]))
-
+      (tool-handle-event (get-selected-tool) event (get-dc))
       (draw-cursor-tile (get-dc) tx ty)
       (resume-flush)
       (set! previous-cursor-tile (list tx ty)))
@@ -240,14 +258,20 @@
     (init-auto-scrollbars (* *world-size* *tile-size*) (* *world-size* *tile-size*) 0 0)))
 
 (define (on-select-tool radio-box event)
+  ;; TODO: change cursor tile decoration?
   (send gui:toplevel-frame set-status-text
         (format "~a" (send radio-box get-selection))))
 
+(define *tools*
+  '(("&Query" query)
+    ("&Bulldoze" bulldoze)
+    ("&Road" build road)
+    ("Zone: Resi&dential" build zone residential)
+    ("Zone: &Commercial" build zone commercial)
+    ("Zone: &Industrial" build zone industrial)))
+
 (define (get-selected-tool)
-  (match (send gui:tool-selection get-selection)
-    [0 '(query)]
-    [1 '(bulldoze)]
-    [2 '(build road)]))
+  (rest (list-ref *tools* (send gui:tool-selection get-selection))))
 
 (define gui:toplevel-frame (new frame% [label "TinyCity"] [width 640] [height 480]))
 (define gui:main-pane (new horizontal-pane% [parent gui:toplevel-frame]))
@@ -258,7 +282,7 @@
 (define gui:tool-selection
   (new radio-box% [label "Selected tool"] [parent gui:sidebar-panel]
        [style '(vertical vertical-label)] [callback on-select-tool]
-       [choices '("&Query" "&Bulldoze" "&Road")]))
+       [choices (map first *tools*)]))
 
 (define gui:main-canvas
   (new main-canvas% [parent gui:main-pane] [toplevel gui:toplevel-frame]))
